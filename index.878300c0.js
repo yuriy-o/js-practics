beb06eb584b95528a6f461cc0f198905db276b96
yuriy-o <82809592+yuriy-o@users.noreply.github.com>
2023-05-04 19:00:51 +0000

    Deploying to gh-pages from @ yuriy-o/js-practics@36d0bd2792e1b4b8a7962c399593ad86187191e9 🚀

--- index.878300c0.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".second-hand"),t=document.querySelector(".min-hand"),n=document.querySelector(".hour-hand");function o(){const o=new Date,r=o.getSeconds(),s=r/60*360+90;e.style.transform=`rotate(${s}deg)`;const c=o.getMinutes(),a=c/60*360+r/60*6+90;t.style.transform=`rotate(${a}deg)`;const d=o.getHours()/12*360+c/60*30+90;n.style.transform=`rotate(${d}deg)`}setInterval(o,1e3),o();
-//# sourceMappingURL=index.878300c0.js.map
--- index.977a52d1.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".second-hand"),t=document.querySelector(".min-hand"),n=document.querySelector(".hour-hand");function r(){var r=new Date,o=r.getSeconds(),a=o/60*360+90;e.style.transform="rotate(".concat(a,"deg)");var c=r.getMinutes(),d=c/60*360+o/60*6+90;t.style.transform="rotate(".concat(d,"deg)");var s=r.getHours()/12*360+c/60*30+90;n.style.transform="rotate(".concat(s,"deg)")}setInterval(r,1e3),r()}();
-//# sourceMappingURL=index.977a52d1.js.map
